06456e2dd3dc23828c6fce7cc4db53865703ba97
michasik <mlukasik@divante.co>
2021-01-12 17:55:44 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="input-wrapper">
+      <label>Max kcal</label>
+      <input type="number" v-model="maxKcal" />
+    </div>
+    <div class="input-wrapper">
+      <label>białko</label>
+      <input type="number" v-model="proteinGrams" />
+    </div>
+    <div class="input-wrapper">
+      <label>tłuszcz</label>
+      <input type="number" v-model="fatGrams" />
+    </div>
+    <div class="input-wrapper">
+      <label>cukry</label>
+      <input type="number" v-model="carbGrams" />
+    </div>
+    <hr>
+    <div class="input-wrapper">
+      <label>Left:</label>
+      <h4>{{ leftKcal }}</h4>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      maxKcal: 1200,
+      fatGrams: 0,
+      proteinGrams: 1,
+      carbGrams: 0,
+    };
+  },
+  computed: {
+    leftKcal() {
+      return this.maxKcal - (this.fatGrams * 9 + 4 * this.proteinGrams + this.carbGrams * 4);
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.input-wrapper {
+  display: flex;
+}
+</style>
