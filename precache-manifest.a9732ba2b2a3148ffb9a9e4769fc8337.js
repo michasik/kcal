605a689b9d7cd7bda56b68fa641c43a018412159
michasik <michas.lukasik@gmail.com>
2021-01-13 09:26:52 +0100

    gh-pages

---
+++ precache-manifest.a9732ba2b2a3148ffb9a9e4769fc8337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a18340b7f3143ab5ef7",
+    "url": "/kcal/css/app.003b6956.css"
+  },
+  {
+    "revision": "d575bcaa6de38c3de97bd2b48a146e29",
+    "url": "/kcal/index.html"
+  },
+  {
+    "revision": "913e21197e486402f44a",
+    "url": "/kcal/js/about.816d1cdc.js"
+  },
+  {
+    "revision": "8a18340b7f3143ab5ef7",
+    "url": "/kcal/js/app.ce39bf60.js"
+  },
+  {
+    "revision": "9a9112111167a0e63aaf",
+    "url": "/kcal/js/chunk-vendors.de53c0a6.js"
+  },
+  {
+    "revision": "8fa91c86dbabfac9839375c637e2dc01",
+    "url": "/kcal/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/kcal/robots.txt"
+  }
+]);
